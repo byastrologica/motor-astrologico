8341b4e131d07dc303424e1c5edc1e03899414ed
byastrologica <byastrologica@gmail.com>
2025-08-25 21:12:54 -0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 // Dicionário de Planetas e Pontos
-const SE_SUN = 0; const SE_MOON = 1;
+const SE_SUN = 0;
+const SE_MOON = 1;
 const SE_MERCURY = 2;
 const SE_VENUS = 3;
 const SE_MARS = 4;
@@ -14,18 +15,19 @@ const SE_CHIRON = 15;
 // Dicionário de Flags (Configurações de Cálculo)
 const SEFLG_SPEED = 256;
 
-// Exportar tudo para que outros arquivos possam usar module.exports = {
- SE_SUN,
- SE_MOON,
- SE_MERCURY,
- SE_VENUS,
- SE_MARS,
- SE_JUPITER,
- SE_SATURN,
- SE_URANUS,
- SE_NEPTUNE,
- SE_PLUTO,
- SE_TRUE_NODE,
- SE_CHIRON,
- SEFLG_SPEED 
+// Exportar tudo para que outros arquivos possam usar
+module.exports = {
+  SE_SUN,
+  SE_MOON,
+  SE_MERCURY,
+  SE_VENUS,
+  SE_MARS,
+  SE_JUPITER,
+  SE_SATURN,
+  SE_URANUS,
+  SE_NEPTUNE,
+  SE_PLUTO,
+  SE_TRUE_NODE,
+  SE_CHIRON,
+  SEFLG_SPEED
 };
