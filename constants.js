e1806054b8d0ae554132f6c2e79c2709b8dc3fa0
byastrologica <byastrologica@gmail.com>
2025-08-27 03:02:33 -0300

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,44 @@
+// Arquivo: constants.js
+
+const planets = {
+  SUN: "sol",
+  MOON: "lua",
+  MERCURY: "mercurio",
+  VENUS: "venus",
+  MARS: "marte",
+  JUPITER: "jupiter",
+  SATURN: "saturno",
+  URANUS: "urano",
+  NEPTUNE: "neptuno",
+  PLUTO: "plutao",
+  NODE: "nodo",
+};
+
+const signs = {
+  ARIES: "aries",
+  TAURUS: "touro",
+  GEMINI: "gemeos",
+  CANCER: "cancer",
+  LEO: "leao",
+  VIRGO: "virgem",
+  LIBRA: "libra",
+  SCORPIO: "escorpiao",
+  SAGITTARIUS: "sagitario",
+  CAPRICORN: "capricornio",
+  AQUARIUS: "aquario",
+  PISCES: "peixes",
+};
+
+const aspects = {
+  CONJUNCTION: "conjuncao",
+  OPPOSITION: "oposicao",
+  TRINE: "trigono",
+  SQUARE: "quadratura",
+  SEXTILE: "sextil",
+};
+
+module.exports = {
+  planets,
+  signs,
+  aspects,
+};
