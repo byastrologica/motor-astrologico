fdecbc39edf72f947beb05f66970c8af8c0b8838
byastrologica <byastrologica@gmail.com>
2025-09-08 20:44:51 -0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -14,7 +14,7 @@ const SE_TRUE_NODE = 11;
 // Dicionário de Flags (Configurações de Cálculo)
 const SEFLG_SPEED = 256;
 
-// Exportar tudo para que outros arquivos possam usar
+// Exportar tudo
 module.exports = {
     SE_SUN, SE_MOON, SE_MERCURY, SE_VENUS, SE_MARS, SE_JUPITER, SE_SATURN,
     SE_URANUS, SE_NEPTUNE, SE_PLUTO, SE_TRUE_NODE, SEFLG_SPEED
