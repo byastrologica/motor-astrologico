dbf895045b33f7738501842f2588dbb96f50a214
byastrologica <byastrologica@gmail.com>
2025-09-10 14:32:01 -0300

    Update mapper.js

--- mapper.js
+++ mapper.js
@@ -1,7 +1,8 @@
-// NOVO: Importa a função que calcula a Dwadasamsa
-const { getDwadasamsaSign } = require('./getDwadasamsaSign');
+// mapper.js
 
-// Dicionários de Tradução para alinhar com os IDs dos CSVs
+const { getDwadasamsaSign } = require('./getDwadasamsaSign');
+const { ZODIAC_SIGNS } = require('./constants'); // Importa ZODIAC_SIGNS
+
 const PLANET_ID_MAP = {
     sun: 'SOL', moon: 'LUA', mercury: 'MERCURIO', venus: 'VENUS', mars: 'MARTE',
     jupiter: 'JUPITER', saturn: 'SATURNO', uranus: 'URANO', neptune: 'NETUNO', pluto: 'PLUTAO'
@@ -12,47 +13,33 @@ const ASPECT_ID_MAP = {
     square: 'QUAD', sextile: 'SEXT'
 };
 
-const ZODIAC_SIGNS = [
-    'ARIES', 'TOURO', 'GEMEOS', 'CANCER', 'LEAO', 'VIRGEM',
-    'LIBRA', 'ESCORPIAO', 'SAGITARIO', 'CAPRICORNIO', 'AQUARIO', 'PEIXES'
-];
-
 let calculatedPlanetsRef = {};
 
+// Esta função agora será usada também pelo index.js
 function getZodiacSign(longitude) {
     const signIndex = Math.floor(longitude / 30);
     const degreeInSign = longitude % 30;
     return {
         name: ZODIAC_SIGNS[signIndex],
-        degree: Math.floor(degreeInSign) + 1
+        degree: Math.floor(degreeInSign) + 1,
+        decimalDegrees: degreeInSign
     };
 }
 
 function mapPlanetToIds(planet, aspects) {
     const { name, longitude } = planet;
-    const { name: signName, degree } = getZodiacSign(longitude);
-    
+    const { name: signName, degree, decimalDegrees } = getZodiacSign(longitude);
     const planetId = PLANET_ID_MAP[name];
-
-    // --- NOVO: Bloco de cálculo da Dwadasamsa ---
-    const degreesInSignDecimal = longitude % 30; // Pega os graus com casas decimais
-    const dwadasamsaSign = getDwadasamsaSign(signName, degreesInSignDecimal);
-    // --- Fim do novo bloco ---
-
-    // Gera os IDs no formato correto do CSV
+    const dwadasamsaSign = getDwadasamsaSign(signName, decimalDegrees);
     const planetSignId = `${planetId}_${signName}`;
     const sabianSymbolId = `${signName}_${degree}`;
-    
     const aspectIds = aspects
         .filter(aspect => aspect.point1 === name || aspect.point2 === name)
         .map(aspect => {
             const otherPlanetName = aspect.point1 === name ? aspect.point2 : aspect.point1;
             const otherPlanetId = PLANET_ID_MAP[otherPlanetName];
             const aspectId = ASPECT_ID_MAP[aspect.aspect_type];
-            
             if (!planetId || !otherPlanetId || !aspectId) return null;
-            
-            // Formato: PLANETA1_ASPECTO_PLANETA2 (ex: SOL_CONJ_LUA)
             return `${planetId}_${aspectId}_${otherPlanetId}`;
         })
         .filter(id => id);
@@ -62,7 +49,7 @@ function mapPlanetToIds(planet, aspects) {
         planetSignId,
         sabianSymbolId,
         aspectIds,
-        dwadasamsaSign // NOVO: Adiciona a informação ao objeto de retorno
+        dwadasamsaSign
     };
 }
 
@@ -70,4 +57,5 @@ function updatePlanetRef(planets) {
     calculatedPlanetsRef = planets;
 }
 
-module.exports = { mapPlanetToIds, updatePlanetRef };
+// ATUALIZADO: Exportamos a função getZodiacSign para uso externo
+module.exports = { mapPlanetToIds, updatePlanetRef, getZodiacSign };
