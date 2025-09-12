06ddc8b9d1406cdee69752e2af29254dd96f914c
byastrologica <byastrologica@gmail.com>
2025-09-11 21:50:52 -0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,5 @@
 // constants.js
 
-// Dicionário de Planetas e Pontos
 const SE_SUN = 0;
 const SE_MOON = 1;
 const SE_MERCURY = 2;
@@ -13,16 +12,13 @@ const SE_NEPTUNE = 8;
 const SE_PLUTO = 9;
 const SE_TRUE_NODE = 11;
 
-// Dicionário de Flags (Configurações de Cálculo)
 const SEFLG_SPEED = 256;
 
-// Array de Signos do Zodíaco
 const ZODIAC_SIGNS = [
   'Áries', 'Touro', 'Gêmeos', 'Câncer', 'Leão', 'Virgem',
   'Libra', 'Escorpião', 'Sagitário', 'Capricórnio', 'Aquário', 'Peixes'
 ];
 
-// Exportar tudo
 module.exports = {
     SE_SUN, SE_MOON, SE_MERCURY, SE_VENUS, SE_MARS, SE_JUPITER, SE_SATURN,
     SE_URANUS, SE_NEPTUNE, SE_PLUTO, SE_TRUE_NODE, SEFLG_SPEED,
