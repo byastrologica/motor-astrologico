a76faa5cd70236bde688ad4090fb2fb9aa73eb32
YCZC <byastrologica@gmail.com>
2025-10-16 15:54:36 -0300

    Add constants for celestial bodies and zodiac signs

---
+++ constants.js
@@ -0,0 +1,26 @@
+// constants.js
+
+const SE_SUN = 0;
+const SE_MOON = 1;
+const SE_MERCURY = 2;
+const SE_VENUS = 3;
+const SE_MARS = 4;
+const SE_JUPITER = 5;
+const SE_SATURN = 6;
+const SE_URANUS = 7;
+const SE_NEPTUNE = 8;
+const SE_PLUTO = 9;
+const SE_TRUE_NODE = 11;
+
+const SEFLG_SPEED = 256;
+
+const ZODIAC_SIGNS = [
+  'Áries', 'Touro', 'Gêmeos', 'Câncer', 'Leão', 'Virgem',
+  'Libra', 'Escorpião', 'Sagitário', 'Capricórnio', 'Aquário', 'Peixes'
+];
+
+module.exports = {
+    SE_SUN, SE_MOON, SE_MERCURY, SE_VENUS, SE_MARS, SE_JUPITER, SE_SATURN,
+    SE_URANUS, SE_NEPTUNE, SE_PLUTO, SE_TRUE_NODE, SEFLG_SPEED,
+    ZODIAC_SIGNS
+};
